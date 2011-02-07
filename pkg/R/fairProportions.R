9070c94e36612895f18962478168c302c7805b2a
ghthomas (Dr ) <gavin.thomas@bristol.ac.uk>
2011-02-07 22:22:40 +0000

    Tweaks to improve memory allocation in fairProportions function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/motmot@18 8ac941fc-44d6-4c39-aea2-23ce9b37b2ad

--- pkg/R/fairProportions.R
+++ pkg/R/fairProportions.R
@@ -2,20 +2,22 @@ fairProportions <- function (phy, nodeCount=FALSE) {
 	
 	treeMatrix <- clade.matrix(phy)
 	
-	fpEdgeVals <- treeMatrix$edge.length / apply(treeMatrix$clade.matrix, 1, sum)  
-	fpEdgeMatrix <- fpEdgeVals * treeMatrix$clade.matrix
-	fpTips <- as.matrix(apply(fpEdgeMatrix, 2, sum))
+	fpEdgeVals <- treeMatrix$edge.length / apply(treeMatrix$clade.matrix, 1, sum) 
+	
+# fpEdgeMatrix <- fpEdgeVals * treeMatrix$clade.matrix
+	
+	fpTips <- as.matrix(apply(fpEdgeVals * treeMatrix$clade.matrix, 2, sum))
 	
 	
 	
 	if (nodeCount==TRUE) {	nodeCount <- apply(treeMatrix$clade.matrix, 2, sum) - 1
-							fpTips <- cbind(fpTips, nodeCount)
-							colnames(fpTips) <- c("FP", "NodeCount")
-							rownames(fpTips) <- phy$tip.label }
+		fpTips <- cbind(fpTips, nodeCount)
+		colnames(fpTips) <- c("FP", "NodeCount")
+		rownames(fpTips) <- phy$tip.label }
 	else { rownames(fpTips) <- phy$tip.label }
-		
-		
-		
+	
+	
+	rm(treeMatrix)
 	return(fpTips)
 	
 }
